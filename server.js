039c47c13e560f17374949949818aa66a0fbf254
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-06-06 23:48:43 +0300

    add mongoose npm, connect to mangoodb

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require("./app");
+const mongoost = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://Nata1:z5YGGcYQGBUQy5sN@cluster0.uzzgdvj.mongodb.net/contacts-db";
+
+mongoost
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
