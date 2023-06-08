8e5aae1fdf3946db2da4a18e3cefa86866c21c06
Наталія Яловега <109685236+NatalisDom@users.noreply.github.com>
2023-06-08 23:25:18 +0300

    confog.js, add .env,

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const app = require("./app");
 const mongoost = require("mongoose");
 
-const DB_HOST =
-  "mongodb+srv://Nata1:z5YGGcYQGBUQy5sN@cluster0.uzzgdvj.mongodb.net/contacts-db";
+
+// const DB_HOST =
+//   "mongodb+srv://Nata1:z5YGGcYQGBUQy5sN@cluster0.uzzgdvj.mongodb.net/contacts-db?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+
+// const { DB_HOST } = process.env;
 
 mongoost
   .connect(DB_HOST)
   .then(() => {
     app.listen(3000, () => {
-      console.log("Server running. Use our API on port: 3000");
+      console.log("Database connection successful");
     });
   })
   .catch((error) => {
